6e54da5468bb52a955e50e494795b968ff763ee1
dition0221 <qjatn8180@naver.com>
2023-05-23 22:50:10 +0900

    23-05-23 / Save & Find & Update to DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,27 +3,54 @@ import Video from "../models/video";
 
 /* Home */
 export const home = async (req, res) => {
+  // DB에서 videos를 불러옴
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
 
 /* Watch Video */
-export const watch = (req, res) => {
-  // id를 가져와서 해당 비디오 찾기
+export const watch = async (req, res) => {
+  // id를 가져와서 DB로부터 해당 비디오 찾기
   const { id } = req.params; // const id = req.params.id;
-  return res.render("watch", { pageTitle: `Watching:` });
+  const video = await Video.findById(id);
+  // if Error
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 /* Edit Video(GET)에서 화면을 보여줌 */
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing:` });
+  const video = await Video.findById(id);
+  // if Error
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 /* Edit Video(POST)에서 변경사항을 저장 */
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  // if Error
+  const video = Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  // Update DB
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) =>
+        word.trim().startsWith("#") ? word.trim() : `#${word.trim()}`
+      ),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -33,7 +60,25 @@ export const getUpload = (req, res) => {
 };
 
 /* Upload Video (POST) */
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  // DB에 저장
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map(
+        (word) =>
+          word.trim().startsWith("#") // '#'기호의 유무 판별
+            ? `#${word.replace(/#/g, "").trim()}` // 모든 '#'기호를 없음으로 대체 후 공백처리
+            : `#${word.trim()}` // 공백처리
+      ),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 /* Video model의 형식(Schema) 지정 */
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  hashtags: [{ type: String }],
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
+  createdAt: { type: Date, default: Date.now, required: true }, // 1970년도부터 지금까지의 ms
+  hashtags: [{ type: String, trim: true }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
 });
 
